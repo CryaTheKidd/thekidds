1a43d5c32d992c9e2675e74f52bd906e3eda4ada
CryaTheKidd <96637307+CryaTheKidd@users.noreply.github.com>
2022-01-09 01:02:18 -0600

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,9 +1,1 @@
 local blacklistedids = 3120144
-
-    if game:GetService("Players").LocalPlayer.UserId == blacklistedids then 
-        game:GetService("Players").LocalPlayer:Kick("BLACKLISTED ACCOUNT") 
-elseif game:GetService("Players").LocalPlayer.UserId ~= settings['userid'] then -- checks if the userid ISNT the userid set in settings
-        game:GetService("Players").LocalPlayer:Kick("NOT RIGHT USERID") 
-    elseif game:GetService("Players").LocalPlayer.UserId == settings['userid'] and game:GetService("Players").LocalPlayer.UserId ~= blacklistedids then -- does sopposite of above
-        print("userid correct loading..") 
-  end
