3511b5b56a39c2f046f952da52b2bcaf2eeca334
CryaTheKidd <96637307+CryaTheKidd@users.noreply.github.com>
2022-01-23 17:18:13 -0600

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,6 +1,9 @@
-local blacklistedids = {
+getgenv().blacklistedids = {
   3006457955,
   1010029524,
   542163985
   }
 print("blacklist ids load")
+print(blacklistedids[1])
+print(blacklistedids[2])
+print(blacklistedids[3])
